70ceb726fe2d6c1a4e888c11f20bdf4d16724d14
ameaya <deobackstep@gmail.com>
2023-09-09 20:15:00 +0600

    update and delete

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Header
 from fastapi.responses import JSONResponse
 from serializers import movieSerializer
 import sqlite3
@@ -8,20 +9,23 @@ app = FastAPI()
 
 
 @app.get('/movies')
-def getMovies(limit: int = -1):
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    if limit == -1:
-        query = 'SELECT movies.id, movies.title, genres.title, movies.release, movies.rating FROM movies ' \
-                'JOIN genres ON movies.genre = genres.id'
+def getMovies(limit: int = -1, token=Header(None)):
+    if token == 'open_sesame':
+        connect = sqlite3.connect('database.db')
+        cursor = connect.cursor()
+        if limit == -1:
+            query = 'SELECT movies.id, movies.title, genres.title, movies.release, movies.rating FROM movies ' \
+                    'JOIN genres ON movies.genre = genres.id'
+        else:
+            query = 'SELECT movies.id, movies.title, genres.title, movies.release, movies.rating FROM movies ' \
+                    'JOIN genres ON movies.genre = genres.id WHERE movies.id <= ' + str(limit)
+        cursor.execute(query)
+        movies = movieSerializer(cursor.fetchall())
+        connect.close()
+        connect.close()
+        return JSONResponse(movies, status_code=200)
     else:
-        query = 'SELECT movies.id, movies.title, genres.title, movies.release, movies.rating FROM movies ' \
-                'JOIN genres ON movies.genre = genres.id WHERE movies.id <= ' + str(limit)
-    cursor.execute(query)
-    movies = movieSerializer(cursor.fetchall())
-    connect.close()
-    connect.close()
-    return JSONResponse(movies, status_code=200)
+        return JSONResponse({'msg': 'token failed!'}, status_code=403)
 
 
 # HOMEWORK 5 September
@@ -49,7 +53,7 @@ def getReleaseMovies(data=Body()):
         connect.close()
         return JSONResponse(movies, status_code=200)
     else:
-        return JSONResponse({'msg': 'Release year is required'}, status_code=403)
+        return JSONResponse({'msg': 'Release year is required'}, status_code=400)
 
 
 @app.post('/movies')
@@ -57,7 +61,7 @@ def addNewMovie(data=Body()):
     required_fields = ['title', 'genre', 'release', 'rating']
     for field in required_fields:
         if field not in data.keys():
-            return JSONResponse({'msg': field + ' is required'}, status_code=403)
+            return JSONResponse({'msg': field + ' is required'}, status_code=400)
     connect = sqlite3.connect('database.db')
     cursor = connect.cursor()
     query = "SELECT id FROM movies ORDER BY id DESC LIMIT 1"
@@ -89,8 +93,76 @@ def addNewMovie(data=Body()):
     return JSONResponse({'msg': 'Movie added successfully'}, status_code=200)
 
 
-# HOMEWORK 7 September
-# Создать ОТДЕЛЬНО в бд новую таблицу actors
-# Данные для таблицы сгенерировать в mockaroo и отправить в таблицу
-# Написать get функцию, которая возвращает список всех записей в этой таблице
-# Написать post функцию, которая принимает данные для добавления новой записи в эту таблицу
+@app.patch('/movies')
+def updateMovie(data=Body()):
+    if 'id' not in data.keys():
+        return JSONResponse({'msg': 'Id is required!'}, status_code=400)
+    else:
+        connect = sqlite3.connect('database.db')
+        cursor = connect.cursor()
+        if 'title' in data.keys():
+            query = "UPDATE movies SET title='" + data['title'] + "' WHERE id=" + str(data['id'])
+            cursor.execute(query)
+        if 'genre' in data.keys():
+            query = "SELECT id FROM genres WHERE title='" + data['genre'] + "'"
+            cursor.execute(query)
+            movie_genre = cursor.fetchone()
+            if movie_genre:
+                query = "UPDATE movies SET genre=" + str(movie_genre[0]) + " WHERE id=" + str(data['id'])
+                cursor.execute(query)
+            else:
+                query = "SELECT id FROM genres ORDER BY id DESC LIMIT 1"
+                cursor.execute(query)
+                movie_genre = str(cursor.fetchone()[0] + 1)
+                query = "INSERT INTO genres(id, title) " \
+                        "VALUES(" + movie_genre + ", '" + data['genre'] + "')"
+                cursor.execute(query)
+                connect.commit()
+                query = "UPDATE movies SET genre=" + movie_genre + " WHERE id=" + str(data['id'])
+                cursor.execute(query)
+        if 'release' in data.keys():
+            query = "UPDATE movies SET release=" + data['release'] + " WHERE id=" + str(data['id'])
+            cursor.execute(query)
+        if 'rating' in data.keys():
+            query = "UPDATE movies SET rating=" + data['rating'] + " WHERE id=" + str(data['id'])
+            cursor.execute(query)
+        connect.commit()
+        query = 'SELECT movies.id, movies.title, genres.title, movies.release, movies.rating FROM movies ' \
+                'JOIN genres ON movies.genre = genres.id WHERE movies.id = ' + str(data['id'])
+        cursor.execute(query)
+        movies = movieSerializer(cursor.fetchall())
+        cursor.close()
+        connect.close()
+        return JSONResponse(movies, status_code=200)
+
+
+@app.delete('/movies')
+def deleteMovie(data=Body()):
+    if 'id' not in data.keys():
+        return JSONResponse({'msg': 'Id is required!'}, status_code=400)
+    else:
+        connect = sqlite3.connect('database.db')
+        cursor = connect.cursor()
+        query = 'DELETE FROM movies WHERE id=' + str(data['id'])
+        cursor.execute(query)
+        connect.commit()
+        cursor.close()
+        connect.close()
+        return JSONResponse({'msg': 'Deleted successfully'}, status_code=200)
+
+
+@app.get('/token')
+def getToken():
+    return JSONResponse({'msg': 'open_sesame'}, status_code=200)
+
+
+# Метод для создания нового жанра
+@app.post('/genre')
+def createGenre():
+    pass
+
+
+# Метод для удаления нового жанра
+@app.delete('/genre')
+def deleteGenre():
+    pass
